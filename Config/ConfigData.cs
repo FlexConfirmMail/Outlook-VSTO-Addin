59262dfb291319404709e6b2df83fac85bb34be5
Fujimoto Seiji <fujimoto@clear-code.com>
2022-03-21 19:51:01 +0900

    Return default values on broken configurations
    Suppose that a configuration file contained a record
    like this:
    
        CountSeconds = aaaa
    
    Previously we returned 0, because we didn't bother to query
    the default value when there is key found in `_options`.
    
    This changes the behavior so that we fall back to the default
    value (in this case, `3`) whenever the user value is broken,
    which should return more safer/reasonable value to the caller.
    
    Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

--- Config/ConfigData.cs
+++ Config/ConfigData.cs
@@ -26,51 +26,93 @@ namespace FlexConfirmMail.Config
             _lists[key] = val;
         }
 
-        public int GetInt(string key)
-        {
-            string val = GetString(key);
-            try
-            {
-                return int.Parse(val);
-            }
-            catch (Exception)
-            {
-                return 0;
-            }
-        }
-
-        public bool GetBool(string key)
-        {
-            string val = GetString(key).ToLower();
-            if (val == "yes" || val == "y" || val == "true" || val == "on")
-            {
-                return true;
-            }
-            else if (val == "no" || val == "n" || val == "false" || val == "off")
-            {
-                return false;
-            }
-            return false;
-        }
-
         public string GetString(string key)
         {
-            if (_options.ContainsKey(key))
+            string val;
+            if (_options.TryGetValue(key, out val))
             {
-                return _options[key];
+                return val;
             }
-            if (_defaults.ContainsKey(key))
+            if (_defaults.TryGetValue(key, out val))
             {
-                return _defaults[key];
+                return val;
             }
             return "";
         }
 
+        public int GetInt(string key)
+        {
+            int val;
+            if (TryGetInt(_options, key, out val))
+            {
+                return val;
+            }
+            if (TryGetInt(_defaults, key, out val))
+            {
+                return val;
+            }
+            return 0;
+        }
+
+        private bool TryGetInt(Dictionary<string, string> dict, string key, out int val)
+        {
+            string raw;
+            if (dict.TryGetValue(key, out raw))
+            {
+                try
+                {
+                    val = int.Parse(raw);
+                    return true;
+                }
+                catch
+                {
+                }
+            }
+            val = 0;
+            return false;
+        }
+
+        public bool GetBool(string key)
+        {
+            bool val;
+            if (TryGetBool(_options, key, out val))
+            {
+                return val;
+            }
+            if (TryGetBool(_defaults, key, out val))
+            {
+                return val;
+            }
+            return false;
+        }
+
+        private bool TryGetBool(Dictionary<string, string> dict, string key, out bool val)
+        {
+            string raw;
+            if (dict.TryGetValue(key, out raw))
+            {
+                raw = raw.ToLower();
+                if (raw == "yes" || raw == "y" || raw == "true" || raw == "on")
+                {
+                    val = true;
+                    return true;
+                }
+                if (raw == "no" || raw == "n" || raw == "false" || raw == "off")
+                {
+                    val = false;
+                    return true;
+                }
+            }
+            val = false;
+            return false;
+        }
+
         public List<string> GetList(string key)
         {
-            if (_lists.ContainsKey(key))
+            List<string> val;
+            if (_lists.TryGetValue(key, out val))
             {
-                return _lists[key];
+                return val;
             }
             return new List<string>();
         }
@@ -82,5 +124,6 @@ namespace FlexConfirmMail.Config
         {
             return new HashSet<string>(GetList(key));
         }
+
     }
 }

86c6ae7cfec04f0a66dfe8d09845d7d60b16d3bf
Fujimoto Seiji <fujimoto@clear-code.com>
2022-03-25 13:33:45 +0900

    FlexConfirmMail v22.0-rc2
    Signed-off-by: Fujimoto Seiji <fujimoto@clear-code.com>

--- Config/Const.cs
+++ Config/Const.cs
@@ -4,7 +4,7 @@ namespace FlexConfirmMail.Config
 {
     public class Global
     {
-        public static readonly string Version = "22.0-rc1";
+        public static readonly string Version = "22.0-rc2";
         public static readonly string AppName = "FlexConfirmMail";
     }
 

eb02c94da706b6cc0e5e1012339055ccec6e1131
Fujimoto Seiji <fujimoto@ceptord.net>
2022-06-06 09:31:55 +0900

    FlexConfirmMail v22.1-rc3
    Signed-off-by: Fujimoto Seiji <fujimoto@ceptord.net>

--- Global.cs
+++ Global.cs
@@ -5,7 +5,7 @@ namespace FlexConfirmMail
     public class Global
     {
         public static readonly string AppName = "FlexConfirmMail";
-        public static readonly string Version = "22.1-rc2";
+        public static readonly string Version = "22.1-rc3";
         public static readonly string Edition = "Enterprise";
         public static readonly bool EnableGPO = true;
     }

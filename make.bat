2769fa65eb085cd495a6e3edb0a1b50fa7888104
Takuro Ashie <ashie@clear-code.com>
2023-09-01 14:58:43 +0900

    Change build & release policy
    Due to changing the release staff, unfortunately the release format
    of v22.2 and v22.3 was different from before. Although it's not
    intentional, we have decided to take this opportunity to change our
    release policy to follow the policy of our other products:
    
    * Don't make differences in functionality between the public version and
      the product version anymore
    * Use self-signed certification instead of ClearCode's one for signing
      the public version of the Add-on
    * Don't sign the public version of the installer anymore

--- make.bat
+++ make.bat
@@ -1,11 +1,3 @@
-@REM Defines the certificate to use for signatures.
-@REM Use Powershell to find the available certs:
-@REM
-@REM PS> Get-ChildItem -Path Cert:CurrentUser\My
-@REM
-set cert=73E7B9D1F72EDA033E7A9D6B17BC37A96CE8513A
-set timestamp=http://timestamp.sectigo.com
-
 @REM ==================
 @REM Compile C# sources
 @REM ==================
@@ -14,14 +6,9 @@ msbuild /p:Configuration=Release
 @REM ==================
 @REM Build an installer
 @REM ==================
-iscc.exe FlexConfirmMail.iss
-
-@REM ==================
-@REM Sign the installer
-@REM ==================
-signtool sign /t %timestamp%  /fd SHA256 /sha1 %cert% dest\FlexConfirmMailSetup*.exe
+iscc.exe /Opublic FlexConfirmMail.iss
 
 @REM ==================
 @REM Compress templates
 @REM ==================
-powershell -C "Compress-Archive  -DestinationPath dest\FlexConfirmMailADMX.zip policy"
+powershell -C "Compress-Archive  -DestinationPath public\FlexConfirmMailADMX.zip policy"

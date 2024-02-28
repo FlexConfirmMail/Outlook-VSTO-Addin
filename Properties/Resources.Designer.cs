54d84c10d1ceae172a5aa9056e5c4ddb005bc544
Takashi Hashida <hashida@clear-code.com>
2024-02-28 17:31:02 +0900

    resources: fix a resource message

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -179,7 +179,7 @@ namespace FlexConfirmMail.Properties {
         }
         
         /// <summary>
-        ///   返信時に新しいドメインが追加されていた場合警告する に類似しているローカライズされた文字列を検索します。
+        ///   返信時に新しいドメインが追加されていた場合に警告する に類似しているローカライズされた文字列を検索します。
         /// </summary>
         public static string ConfigSafeNewDomainsEnabled {
             get {

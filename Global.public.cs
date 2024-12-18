0ccb08225bbc645b4aa56adeea00de663d67e252
Takashi Hashida <hashida@clear-code.com>
2024-12-18 13:27:08 +0900

    Bump version to 22.4.3 (#46)

--- Global.public.cs
+++ Global.public.cs
@@ -5,7 +5,7 @@ namespace FlexConfirmMail
     public class Global
     {
         public static readonly string AppName = "FlexConfirmMail";
-        public static readonly string Version = "22.4.2";
+        public static readonly string Version = "22.4.3";
         public static readonly string Edition = "Free";
         public static readonly bool EnableGPO = false;
     }
